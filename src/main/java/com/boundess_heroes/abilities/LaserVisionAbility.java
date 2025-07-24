75e6e7654cc603274b1e525195733b0195e22526
JRepo42 <JRepo42@users.noreply.github.com>
2025-07-24 01:12:13 +0100

    Small heat vision progress made (does dmg)

--- src/main/java/com/boundess_heroes/abilities/LaserVisionAbility.java
+++ src/main/java/com/boundess_heroes/abilities/LaserVisionAbility.java
@@ -1,31 +1,41 @@
 package com.boundess_heroes.abilities;
 
 import com.boundess_heroes.BoundlessHeroes;
+import com.boundless.action.Action;
+import com.boundless.entity.hero_action.HeroActionEntity;
+import com.boundless.util.ActionUtils;
+import com.boundless.util.RaycastUtils;
+import mod.chloeprime.aaaparticles.api.client.effekseer.ParticleEmitter;
 import mod.chloeprime.aaaparticles.api.common.AAALevel;
 import mod.chloeprime.aaaparticles.api.common.ParticleEmitterInfo;
+import mod.chloeprime.aaaparticles.client.registry.EffectRegistry;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.hit.EntityHitResult;
+
+import java.util.LinkedHashMap;
+import java.util.function.BiConsumer;
 
 public class LaserVisionAbility {
     public static void laserVisionLogic(PlayerEntity player) {
         ParticleEmitterInfo laserParticle = ParticleEmitterInfo.create(player.getWorld(), BoundlessHeroes.identifier("laser"), BoundlessHeroes.identifier("laser_" + player.getName()));
-        laserParticle.entitySpaceRelativePosition(-0.15f, player.getEyeHeight(player.getPose()), 0);
-        laserParticle.bindOnEntity(player);
-        //laserParticle.rotation((float) Math.toRadians(player.getPitch() * -1), (float) Math.toRadians(player.getYaw() -1), 0);
         laserParticle.scale(0.1f);
         laserParticle.parameter(0, 5);
         AAALevel.addParticle(player.getWorld(), laserParticle);
 
-        /*
         LinkedHashMap<Integer, BiConsumer<PlayerEntity, HeroActionEntity>> tasks = new LinkedHashMap<>();
         tasks.put(1000, (user, heroAction) -> heroAction.discard());
 
         BiConsumer<PlayerEntity, HeroActionEntity> tickLogic = (user, heroAction) -> {
-
+            if (player.age % 2 != 0) return;
+            EntityHitResult entityRaycast = RaycastUtils.raycast(player, 64);
+            if (entityRaycast != null && entityRaycast.getEntity() instanceof LivingEntity livingEntity) {
+                livingEntity.timeUntilRegen = 0;
+                livingEntity.damage(livingEntity.getDamageSources().generic(), 1.0f);
+            }
         };
 
-
-        Action laserAction = Action.builder().customTickLogic().build();
-
-         */
+        Action laserAction = Action.builder().customTickLogic(tickLogic).scheduledTasks(tasks).build();
+        ActionUtils.performAction(player, laserAction);
     }
 }
